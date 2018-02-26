84a02fd57ed8e85a01e5b974c3f14d57f0cb8889
Rob Thuleen <rob.thuleen@gmail.com>
2018-02-25 19:32:32 -0800

    draw variable number of verticies

--- chaos.js
+++ chaos.js
@@ -1,22 +1,36 @@
-function Chaos(canvasId, times) {
+function Chaos(canvasId) {
+    this.times = parseInt(document.getElementById('times').value, 10);
+    this.sides = parseInt(document.getElementById('sides').value, 10);
     this.canvas = document.getElementById(canvasId);
     this.ctx = canvas.getContext("2d");
+    this.ctx.clearRect(0,0,this.width, this.height);
     this.pointSize = 1;
     this.height = this.canvas.height - this.pointSize;
     this.width = this.canvas.width - this.pointSize;
-    this.times = times;
-    this.verticies = [
-        new Point(0, 0),
-        new Point(this.width / 2, this.height),
-        new Point(this.width, 0)
-    ]
+    this.verticies = [];
+    this.makeVerticies(this.points);
 }
+
+Chaos.prototype.makeVerticies = function (n) {
+    var radius = Math.min(this.width, this.height)/2;
+    var angle = 2*Math.PI/this.sides;
+    var rot = Math.PI/2;
+    for (var point = 0; point < this.sides; point++) {
+        var x = radius*Math.cos(angle*point+rot);
+        var y = radius*Math.sin(angle*point+rot);
+        x = x + radius;
+        y = y + radius;
+        this.verticies.push(new Point(x,y));
+    }
+}
+
 
 Chaos.prototype.plot = function (point) {
     this.ctx.strokeRect(point.x, point.y, this.pointSize, this.pointSize);
 }
 
 Chaos.prototype.start = function () {
+    this.ctx.clearRect(0,0,this.width, this.height);
     var _this = this;
     this.verticies.forEach(function (vertex) {
         _this.plot(vertex);
@@ -30,7 +44,7 @@ Chaos.prototype.start = function () {
     var times = 0;
     for (var times = 0; times < this.times; times++) {
         var lastPoint = newPoint;
-        var chaos = Math.floor(Math.random() * 3);
+        var chaos = Math.floor(Math.random() * this.verticies.length);
         var vertex = this.verticies[chaos];
         var newPoint = lastPoint.midPoint(vertex);
         this.plot(newPoint);
